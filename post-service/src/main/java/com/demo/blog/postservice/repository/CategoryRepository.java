fb6ff0aa59e819c9cbe38ed5f0d46e9d45cee82f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-14 17:37:53 +0200

    Change return type

--- post-service/src/main/java/com/demo/blog/postservice/repository/CategoryRepository.java
+++ post-service/src/main/java/com/demo/blog/postservice/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.demo.blog.postservice.repository;
 import com.demo.blog.postservice.domain.Category;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends MongoRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
 }
