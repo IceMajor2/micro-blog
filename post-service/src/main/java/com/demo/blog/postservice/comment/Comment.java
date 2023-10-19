ed150458b9a14ab6cb8bcdfd4af846aff5b52fed
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-19 14:15:21 +0200

    Create comment model

---
+++ post-service/src/main/java/com/demo/blog/postservice/comment/Comment.java
@@ -0,0 +1,25 @@
+package com.demo.blog.postservice.comment;
+
+import com.demo.blog.postservice.post.Post;
+import lombok.Data;
+import org.springframework.data.annotation.Transient;
+
+import java.time.LocalDateTime;
+
+@Data
+public final class Comment {
+
+    private String username;
+    private String body;
+    private LocalDateTime publishedOn;
+    private LocalDateTime updatedOn;
+
+    @Transient
+    private Post post;
+
+    public Comment(String username, String body) {
+        this.username = username;
+        this.body = body;
+        this.publishedOn = LocalDateTime.now();
+    }
+}
