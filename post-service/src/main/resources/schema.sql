26ff27596f8e51b2e8440e3980c1755d410a16a8
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-19 13:32:50 +0200

    Specify database schema

---
+++ post-service/src/main/resources/schema.sql
@@ -0,0 +1,46 @@
+-- FOR TESTING PURPOSES
+DROP TABLE IF EXISTS post_category;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS author;
+DROP TABLE IF EXISTS category;
+-- END
+
+CREATE TABLE IF NOT EXISTS author (
+    id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username    VARCHAR(32) NOT NULL,-- UNIQUE
+    email   VARCHAR(96) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS post (
+    id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title   VARCHAR(255) NOT NULL,
+    body    TEXT NOT NULL,
+    published_on    TIMESTAMP NOT NULL,
+    updated_on  TIMESTAMP,
+    author  BIGINT,
+    FOREIGN KEY (author) REFERENCES author(id)
+);
+
+CREATE TABLE IF NOT EXISTS category (
+    id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name    VARCHAR(32) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS post_category (
+    id  BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    post_id BIGINT NOT NULL,
+    category_id BIGINT NOT NULL,
+    UNIQUE (post_id, category_id),
+    FOREIGN KEY (post_id) REFERENCES post(id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE IF NOT EXISTS comment (
+    post    BIGINT NOT NULL,
+    username    VARCHAR(32) NOT NULL,-- UNIQUE
+    body    TEXT NOT NULL,
+    published_on    TIMESTAMP NOT NULL,
+    updated_on  TIMESTAMP,
+    FOREIGN KEY (post) REFERENCES post(id)
+);
