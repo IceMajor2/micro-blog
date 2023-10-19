c487a447865f9dea2615bd542174dc187082005f
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-19 15:07:50 +0200

    Set error http code & message

--- post-service/src/main/java/com/demo/blog/postservice/category/exception/CategoryNotFoundException.java
+++ post-service/src/main/java/com/demo/blog/postservice/category/exception/CategoryNotFoundException.java
@@ -1,4 +1,14 @@
 package com.demo.blog.postservice.category.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static java.lang.StringTemplate.STR;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CategoryNotFoundException extends RuntimeException {
+
+    public CategoryNotFoundException(String categoryName) {
+        super(STR."Category '\{ categoryName }' was not found");
+    }
 }
