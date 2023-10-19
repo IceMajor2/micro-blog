1fba80074c6b88e40cef2a0d776724e38cedd27d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-19 15:08:10 +0200

    Restructure to a DTO

--- post-service/src/main/java/com/demo/blog/postservice/category/dto/CategoryRequest.java
+++ post-service/src/main/java/com/demo/blog/postservice/category/dto/CategoryRequest.java
@@ -1,17 +1,10 @@
 package com.demo.blog.postservice.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Builder
-public class CategoryRequest {
-
-    @NotBlank(message = "Name must be specified")
-    private String name;
+public record CategoryRequest(
+        @NotBlank(message = "Name must be specified") String name
+) {
 }
