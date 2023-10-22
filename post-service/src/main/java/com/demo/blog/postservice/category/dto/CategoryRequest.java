09555643bd17273fa38c045b8acd4f1c8274d516
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-22 08:38:57 +0200

    Add constraint for name length upper bound

--- post-service/src/main/java/com/demo/blog/postservice/category/dto/CategoryRequest.java
+++ post-service/src/main/java/com/demo/blog/postservice/category/dto/CategoryRequest.java
@@ -2,9 +2,12 @@ package com.demo.blog.postservice.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 public record CategoryRequest(
-        @NotBlank(message = "Name must be specified") String name
+        @NotBlank(message = "Name must be specified")
+        @Length(max = 32, message = "Category name cannot exceed 32 characters")
+        String name
 ) {
 }
