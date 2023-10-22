ca3a3a6e10916ac1914a7f6a815846b09776d3b4
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-22 13:13:11 +0200

    Create SQL script with test data

---
+++ post-service/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO category (ID, NAME) VALUES (1, 'Spring');
+INSERT INTO category (ID, NAME) VALUES (2, 'Concurrency');
+INSERT INTO category (ID, NAME) VALUES (3, 'Testing');
+INSERT INTO category (ID, NAME) VALUES (4, 'Software Engineering');
+INSERT INTO category (ID, NAME) VALUES (5, 'C');
+INSERT INTO category (ID, NAME) VALUES (6, 'Low-level');
