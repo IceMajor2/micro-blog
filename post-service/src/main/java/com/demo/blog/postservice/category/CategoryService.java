77ecc1c6f37dd03a5f8d663b55c5b1ee04c87aa3
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-22 10:07:02 +0200

    Create a CategoryService interface and implement it; add transaction support

--- post-service/src/main/java/com/demo/blog/postservice/category/CategoryService.java
+++ post-service/src/main/java/com/demo/blog/postservice/category/CategoryService.java
@@ -2,45 +2,20 @@ package com.demo.blog.postservice.category;
 
 import com.demo.blog.postservice.category.dto.CategoryRequest;
 import com.demo.blog.postservice.category.dto.CategoryResponse;
-import com.demo.blog.postservice.category.exception.CategoryAlreadyExistsException;
-import com.demo.blog.postservice.category.exception.CategoryNotFoundException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Set;
 
-@Service
-@RequiredArgsConstructor
-public class CategoryService {
+interface CategoryService {
 
-    private final CategoryRepository categoryRepository;
+    CategoryResponse getById(Long id);
 
-    CategoryResponse getByName(String categoryName) {
-        if (categoryName == null)
-            throw new NullPointerException("Category name was null");
-        return new CategoryResponse(categoryRepository.findByName(categoryName)
-                .orElseThrow(() -> new CategoryNotFoundException(categoryName)));
-    }
+    CategoryResponse getByName(String name);
 
-    CategoryResponse getById(Long id) {
-        if (id == null)
-            throw new NullPointerException("Category ID was null");
-        return new CategoryResponse(categoryRepository.findById(id)
-                .orElseThrow(() -> new CategoryNotFoundException(id)));
-    }
+    Set<CategoryResponse> getAll();
 
-    List<CategoryResponse> getAll() {
-        return categoryRepository.findAll().stream()
-                .map(CategoryResponse::new)
-                .toList();
-    }
+    CategoryResponse add(CategoryRequest request);
 
-    CategoryResponse add(CategoryRequest request) {
-        Category category = new CategoryBuilder()
-                .fromRequest(request)
-                .build();
-        if (categoryRepository.existsByName(category.getName()))
-            throw new CategoryAlreadyExistsException(category.getName());
-        return new CategoryResponse(categoryRepository.save(category));
-    }
+    CategoryResponse replace(CategoryRequest request);
+
+    CategoryResponse delete(Long id);
 }
---
+++ post-service/src/main/java/com/demo/blog/postservice/category/CategoryServiceImpl.java
@@ -0,0 +1,66 @@
+package com.demo.blog.postservice.category;
+
+import com.demo.blog.postservice.category.dto.CategoryRequest;
+import com.demo.blog.postservice.category.dto.CategoryResponse;
+import com.demo.blog.postservice.category.exception.CategoryAlreadyExistsException;
+import com.demo.blog.postservice.category.exception.CategoryNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public CategoryResponse getByName(String categoryName) {
+        if (categoryName == null)
+            throw new NullPointerException("Category name was null");
+        return new CategoryResponse(categoryRepository.findByName(categoryName)
+                .orElseThrow(() -> new CategoryNotFoundException(categoryName)));
+    }
+
+    @Override
+    public CategoryResponse getById(Long id) {
+        if (id == null)
+            throw new NullPointerException("Category ID was null");
+        return new CategoryResponse(categoryRepository.findById(id)
+                .orElseThrow(() -> new CategoryNotFoundException(id)));
+    }
+
+    @Override
+    public Set<CategoryResponse> getAll() {
+        return categoryRepository.findAll().stream()
+                .map(CategoryResponse::new)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public CategoryResponse add(CategoryRequest request) {
+        Category category = new CategoryBuilder()
+                .fromRequest(request)
+                .build();
+        if (categoryRepository.existsByName(category.getName()))
+            throw new CategoryAlreadyExistsException(category.getName());
+        return new CategoryResponse(categoryRepository.save(category));
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public CategoryResponse replace(CategoryRequest request) {
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public CategoryResponse delete(Long id) {
+        return null;
+    }
+}
