98e0e82609485f404741a49fb922f4cb40d30294
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-22 10:17:30 +0200

    Add a sorted set of category objects

--- post-service/src/test/java/com/demo/blog/postservice/category/CategoryDataSupply.java
+++ post-service/src/test/java/com/demo/blog/postservice/category/CategoryDataSupply.java
@@ -2,9 +2,25 @@ package com.demo.blog.postservice.category;
 
 import com.demo.blog.postservice.category.dto.CategoryRequest;
 
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.demo.blog.postservice.category.CategoryServiceTest.ANY_LONG;
+
 public class CategoryDataSupply {
+
+    static Set<Category> sortedCategories() {
+        return categoryNames()
+                .map(name -> new CategoryBuilder()
+                        .withId(ANY_LONG)
+                        .withName(name)
+                        .build())
+                .sorted(Comparator.comparing(Category::getName))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
 
     static Stream<String> categoryNames() {
         return Stream.of(
