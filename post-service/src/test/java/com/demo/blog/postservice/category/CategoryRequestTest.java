54ebc50262ca6e62aeb22b9215c6e90f19f6a7fe
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-22 08:38:24 +0200

    Write a test asserting the length of category name cannot be bigger than 32 chars

--- post-service/src/test/java/com/demo/blog/postservice/category/CategoryRequestTest.java
+++ post-service/src/test/java/com/demo/blog/postservice/category/CategoryRequestTest.java
@@ -15,14 +15,26 @@ import static com.demo.blog.postservice.assertions.JakartaValidationAssert.asser
 public class CategoryRequestTest {
 
     private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-    private static final String NOT_BLANK_MSG = "Name must be specified";
+    private static final String EXP_NOT_BLANK_MSG = "Name must be specified";
+    private static final String EXP_NAME_TOO_LONG_MSG = "Category name cannot exceed 32 characters";
 
     @ParameterizedTest
     @ValueSource(strings = {"", "  \t  \n\t", "      "})
     @NullSource
     void shouldThrowExceptionOnBlankCategory(String categoryName) {
-        CategoryRequest request = CategoryRequest.builder().name(categoryName).build();
-        assertThat(validate(request)).containsOnlyExceptionMessages(NOT_BLANK_MSG);
+        CategoryRequest request = new CategoryRequest(categoryName);
+        assertThat(validate(request)).containsOnlyExceptionMessages(EXP_NOT_BLANK_MSG);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "THIRTY_THREE_CHARS_STRING_REQUEST",
+            "_THIRTY_MORE_CHARS_STRING_REQUEST_",
+            "_THIRTY_MORE_CHARS_STRING_REQUEST__"
+    })
+    void shouldThrowExceptionOnCategoryNameLongerThan32Chars(String tooLongString) {
+        CategoryRequest request = new CategoryRequest(tooLongString);
+        assertThat(validate(request)).containsOnlyExceptionMessages(EXP_NAME_TOO_LONG_MSG);
     }
 
     private static Set<ConstraintViolation<Object>> validate(Object object) {
