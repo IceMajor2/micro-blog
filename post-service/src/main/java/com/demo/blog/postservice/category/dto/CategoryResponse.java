f669f88cf1fe55090888e672ed05c0ed5ebfc2c1
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-25 16:54:00 +0200

    Override equals and hash code

--- post-service/src/main/java/com/demo/blog/postservice/category/dto/CategoryResponse.java
+++ post-service/src/main/java/com/demo/blog/postservice/category/dto/CategoryResponse.java
@@ -2,12 +2,28 @@ package com.demo.blog.postservice.category.dto;
 
 import com.demo.blog.postservice.category.Category;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+
+import java.util.Objects;
 
 public record CategoryResponse(
-        @JsonIgnore Long id,
+        @JsonIgnore @EqualsAndHashCode.Exclude Long id,
         String name
 ) {
     public CategoryResponse(Category category) {
         this(category.getId(), category.getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof CategoryResponse)) return false;
+        CategoryResponse that = (CategoryResponse) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
