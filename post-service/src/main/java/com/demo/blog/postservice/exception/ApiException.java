5a7ecde13216f138ebf93385bd6c352c4e91034d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-26 10:55:39 +0200

    Store status as HttpStatus instance not just integer + add a constructor with DTO as param

--- post-service/src/main/java/com/demo/blog/postservice/exception/ApiException.java
+++ post-service/src/main/java/com/demo/blog/postservice/exception/ApiException.java
@@ -1,33 +1,34 @@
 package com.demo.blog.postservice.exception;
 
-import com.fasterxml.jackson.annotation.JsonIncludeProperties;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-// the annotation below prevents from showing additional fields of 'RuntimeException' class
-@JsonIncludeProperties({"timestamp", "status", "error", "message", "path"})
-@JsonPropertyOrder({"timestamp", "status", "error", "message", "path"})
 @Data
 public class ApiException extends RuntimeException {
 
     private String timestamp;
-    private int status;
+    private HttpStatus status;
     private String error;
     private String message;
     private String path;
 
-    @Autowired
     public ApiException(HttpStatus status, String message, String path) {
         this.timestamp = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now(Clock.systemUTC()));
-        this.status = status.value();
+        this.status = status;
         this.error = status.getReasonPhrase();
         this.message = message;
         this.path = path;
+    }
+
+    public ApiException(ApiExceptionDTO exceptionDTO) {
+        this.timestamp = exceptionDTO.getTimestamp();
+        this.status = HttpStatus.valueOf(exceptionDTO.getStatus());
+        this.error = exceptionDTO.getError();
+        this.message = exceptionDTO.getMessage();
+        this.path = exceptionDTO.getPath();
     }
 }
