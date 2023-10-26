88884790d531b10a1137e6b85dd66ea79cd1c690
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-26 10:56:47 +0200

    Include RestExceptionAssert

--- post-service/src/test/java/com/demo/blog/postservice/assertions/AllAssertions.java
+++ post-service/src/test/java/com/demo/blog/postservice/assertions/AllAssertions.java
@@ -1,6 +1,7 @@
 package com.demo.blog.postservice.assertions;
 
 import com.demo.blog.postservice.category.dto.CategoryResponse;
+import com.demo.blog.postservice.exception.ApiExceptionDTO;
 import jakarta.validation.ConstraintViolation;
 import org.assertj.core.api.Assertions;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,10 @@ public class AllAssertions extends Assertions {
 
     public static HttpResponseAssert assertThatResponse(ResponseEntity<?> actual) {
         return HttpResponseAssert.assertThat(actual);
+    }
+
+    public static RestExceptionAssert assertThatException(ResponseEntity<ApiExceptionDTO> actual) {
+        return RestExceptionAssert.assertThatException(actual);
     }
 
     public static JakartaValidationAssert assertThat(Set<ConstraintViolation<Object>> actual) {
