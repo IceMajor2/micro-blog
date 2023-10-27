b14d9b8553fcec5401714c5174359fe4c77bc388
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-27 13:17:34 +0200

    Include category-specific custom matchers in here

---
+++ post-service/src/test/java/com/demo/blog/postservice/category/assertion/CategoryAssert.java
@@ -0,0 +1,28 @@
+package com.demo.blog.postservice.category.assertion;
+
+import com.demo.blog.postservice.category.dto.CategoryResponse;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.Assertions;
+
+import java.util.Comparator;
+
+public class CategoryAssert extends AbstractAssert<CategoryAssert, CategoryResponse> {
+
+    public static final Comparator<CategoryResponse> CATEGORY_RESPONSE_COMPARATOR = Comparator.comparing(CategoryResponse::name);
+
+    public CategoryAssert(CategoryResponse actual) {
+        super(actual, CategoryAssert.class);
+    }
+
+    public static CategoryAssert assertThat(CategoryResponse actual) {
+        return new CategoryAssert(actual);
+    }
+
+    public CategoryAssert isNamed(String name) {
+        isNotNull();
+        Assertions.assertThat(actual.name())
+                .as("name")
+                .isEqualTo(name);
+        return this;
+    }
+}
