599a77bbce9e6a2e73d22068bdd013b1b1425d04
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-10-27 13:37:06 +0200

    Add global handling for ConstraintViolationException

--- post-service/src/main/java/com/demo/blog/postservice/exception/GlobalExceptionHandler.java
+++ post-service/src/main/java/com/demo/blog/postservice/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.demo.blog.postservice.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
@@ -14,6 +17,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -29,8 +33,29 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.badRequest().body(new ApiExceptionDTO(exception));
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException e,
+                                                                        WebRequest request) {
+        String path = getPath(request);
+        String message = getMessage(e.getConstraintViolations());
+        ApiException exception = new ApiException(HttpStatus.BAD_REQUEST, message, path);
+        return ResponseEntity.badRequest().body(new ApiExceptionDTO(exception));
+    }
+
     private String getPath(WebRequest request) {
         return ((ServletWebRequest) request).getRequest().getRequestURI();
+    }
+
+    private String getMessage(Set<ConstraintViolation<?>> violations) {
+        List<String> messages = new ArrayList<>();
+        var iterator = violations.stream().iterator();
+        while (iterator.hasNext()) {
+            var violation = iterator.next();
+            messages.add(violation.getMessage());
+        }
+
+        Collections.sort(messages);
+        return String.join("; ", messages);
     }
 
     private String getMessage(List<FieldError> violations) {
