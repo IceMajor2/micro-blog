3e06b5f067f7c82c5850db4c7674e2b886e991ef
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-01 10:15:46 +0100

    Add integration test asserting deleting a non-existing id throws exception

--- post-service/src/test/java/com/demo/blog/postservice/category/integration/CategoryHttpDeleteTest.java
+++ post-service/src/test/java/com/demo/blog/postservice/category/integration/CategoryHttpDeleteTest.java
@@ -2,16 +2,20 @@ package com.demo.blog.postservice.category.integration;
 
 import com.demo.blog.postservice.category.CategoryRepository;
 import com.demo.blog.postservice.category.dto.CategoryResponse;
+import com.demo.blog.postservice.exception.ApiExceptionDTO;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import static com.demo.blog.postservice.assertion.AllAssertions.assertThat;
-import static com.demo.blog.postservice.category.datasupply.Constants.API_CATEGORY_ID;
+import static com.demo.blog.postservice.assertion.AllAssertions.assertThatException;
+import static com.demo.blog.postservice.category.datasupply.Constants.*;
 import static com.demo.blog.postservice.util.RestRequestUtils.delete;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -38,5 +42,18 @@ public class CategoryHttpDeleteTest {
         assertThat(actualSize)
                 .withFailMessage("Category was not deleted from the repository")
                 .isEqualTo(expectedSize);
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {-10, 0, 1000})
+    void shouldThrowExceptionOnDeletingIdNotFound(Long deleteId) {
+        // act
+        var actual = delete(API_CATEGORY_ID, ApiExceptionDTO.class, deleteId);
+
+        // assert
+        assertThatException(actual)
+                .isNotFound()
+                .withMessage(ID_NOT_FOUND_MSG_T.formatted(deleteId))
+                .withPath(API_CATEGORY_SLASH + deleteId);
     }
 }
