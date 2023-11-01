a307872c80f0102fd5df0304fba494191571e68a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-01 14:18:51 +0100

    Create Spring Boot project for Post service

---
+++ post-service/src/test/java/com/demo/blog/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.blog.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
