f021085c901665bc1eb0d1d78e872125bd70b095
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-01 09:51:32 +0100

    Implement a few equals test methods

--- post-service/src/test/java/com/demo/blog/postservice/category/CategoryResponseTest.java
+++ post-service/src/test/java/com/demo/blog/postservice/category/CategoryResponseTest.java
@@ -3,7 +3,10 @@ package com.demo.blog.postservice.category;
 import com.demo.blog.postservice.category.dto.CategoryResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Objects;
 
 import static com.demo.blog.postservice.assertion.AllAssertions.assertThat;
 
@@ -26,10 +29,27 @@ class CategoryResponseTest {
 
     @Test
     void equalsShouldReturnTrueOnEqualObjects() {
+        // arrange
+        CategoryResponse response1 = new CategoryResponse(1L, "Java");
+        CategoryResponse response2 = new CategoryResponse(1L, "Java");
+
+        // act & assert
+        assertThat(Objects.equals(response1, response2)).isTrue();
     }
 
-    @Test
-    void equalsShouldReturnFalseOnDifferingObjects() {
+    @ParameterizedTest
+    @CsvSource({
+            "1,Threads,2,Threads",
+            "10,C,10,C++",
+            "5,Learning,3,Discipline"
+    })
+    void equalsShouldReturnFalseOnDifferingObjects(Long id1, String name1, Long id2, String name2) {
+        // arrange
+        CategoryResponse response1 = new CategoryResponse(id1, name1);
+        CategoryResponse response2 = new CategoryResponse(id2, name2);
+
+        // act & assert
+        assertThat(Objects.equals(response1, response2)).isFalse();
     }
 
     @Test
