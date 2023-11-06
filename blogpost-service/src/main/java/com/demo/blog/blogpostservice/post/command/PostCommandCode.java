d711230af7282e3cc13af2e1faca3a03ddf1ba5c
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-06 09:55:17 +0100

    Add Lombok constructor

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/PostCommandCode.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/PostCommandCode.java
@@ -1,16 +1,14 @@
 package com.demo.blog.blogpostservice.post.command;
 
 import com.demo.blog.blogpostservice.command.CommandCode;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public enum PostCommandCode implements CommandCode {
 
     GET_ALL_POSTS("P_1");
 
     final String code;
-
-    PostCommandCode(String code) {
-        this.code = code;
-    }
 
     @Override
     public String getCode() {
