c219575e11c03a58a3d095f4ffc6d39c8c1b3d57
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-06 11:46:44 +0100

    Add new fields to PostResponse

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/dto/PostResponse.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/dto/PostResponse.java
@@ -1,10 +1,24 @@
 package com.demo.blog.blogpostservice.post.dto;
 
+import com.demo.blog.blogpostservice.author.dto.AuthorResponse;
+import com.demo.blog.blogpostservice.category.dto.CategoryResponse;
+import com.demo.blog.blogpostservice.post.Post;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
 
 public record PostResponse(
         @JsonIgnore Long id,
         String title,
+        AuthorResponse author,
+        List<CategoryResponse> categories,
+        String publishedOn,
+        @JsonInclude(JsonInclude.Include.NON_NULL) String updatedOn,
         String body
 ) {
+    public PostResponse(Post post) {
+        this(post.getId(), post.getTitle(), null, null,
+                post.getPublishedOn().toString(), null, post.getBody());
+    }
 }
