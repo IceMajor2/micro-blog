3e904f9f5abc3b485dd8cd7bb06e292a720a3688
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-06 10:02:55 +0100

    Add test for command factory returning correct command instance

---
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/command/CommandFactoryTest.java
@@ -0,0 +1,38 @@
+package com.demo.blog.blogpostservice.command;
+
+import com.demo.blog.blogpostservice.post.PostRepository;
+import com.demo.blog.blogpostservice.post.command.GetAllPostsCommand;
+import com.demo.blog.blogpostservice.post.command.PostCommandCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.demo.blog.blogpostservice.assertion.AllAssertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+@ExtendWith(MockitoExtension.class)
+class CommandFactoryTest {
+
+    private CommandFactory SUT;
+
+    private PostRepository postRepository;
+
+    @BeforeEach
+    void setUp() {
+        postRepository = mock(PostRepository.class);
+        SUT = new CommandFactory(postRepository);
+    }
+
+    @Test
+    void shouldReturnCorrectCommand() {
+        // act
+        Command actual = SUT.create(PostCommandCode.GET_ALL_POSTS);
+
+        // assert
+        assertThat(actual).isInstanceOf(GetAllPostsCommand.class);
+    }
+}
