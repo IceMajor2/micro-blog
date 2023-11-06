fec9815b3232ab822b020dd6d6d36875bb189df0
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-06 09:54:49 +0100

    Make 'code' variable final

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/PostCommandCode.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/PostCommandCode.java
@@ -3,9 +3,10 @@ package com.demo.blog.blogpostservice.post.command;
 import com.demo.blog.blogpostservice.command.CommandCode;
 
 public enum PostCommandCode implements CommandCode {
+
     GET_ALL_POSTS("P_1");
 
-    String code;
+    final String code;
 
     PostCommandCode(String code) {
         this.code = code;
