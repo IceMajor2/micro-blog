753a735eb05b7198eed9c3371bf518c7818cb964
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-08 13:37:11 +0100

    Add posts field

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/author/Author.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/author/Author.java
@@ -1,15 +1,22 @@
 package com.demo.blog.blogpostservice.author;
 
+import com.demo.blog.blogpostservice.post.Post;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
+@AllArgsConstructor
 public final class Author {
 
     @Id
     private Long id;
     private String username;
     private String email;
+    private Set<Post> posts = new HashSet<>();
 
     public Author(String username, String email) {
         this.username = username;
