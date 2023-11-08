631932496b384c2db7b34eb9707fc2c0f9e3e3fd
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-08 13:50:24 +0100

    Move common constants to separate class

---
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/datasupply/Constants.java
@@ -0,0 +1,9 @@
+package com.demo.blog.blogpostservice.datasupply;
+
+public class Constants {
+
+    public static final Long ANY_LONG = 1L;
+    public static final Long NEGATIVE_LONG = -1L;
+    public static final String ANY_STRING = "ANY_STRING";
+    public static final String ANY_EMAIL = "EMAIL@EMAIL.COM";
+}
