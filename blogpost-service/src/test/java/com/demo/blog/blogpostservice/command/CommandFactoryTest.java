588ea75daabc9c8a110d65bf46f07f610c02ac34
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-08 22:33:09 +0100

    Mock using annotations + add another DOC

--- blogpost-service/src/test/java/com/demo/blog/blogpostservice/command/CommandFactoryTest.java
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/command/CommandFactoryTest.java
@@ -1,34 +1,33 @@
 package com.demo.blog.blogpostservice.command;
 
 import com.demo.blog.blogpostservice.author.AuthorRepository;
+import com.demo.blog.blogpostservice.category.CategoryRepository;
 import com.demo.blog.blogpostservice.post.PostRepository;
 import com.demo.blog.blogpostservice.post.command.GetAllPostsCommand;
 import com.demo.blog.blogpostservice.post.command.PostCommandCode;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static com.demo.blog.blogpostservice.assertion.AllAssertions.assertThat;
-import static org.mockito.Mockito.mock;
 
 @TestMethodOrder(MethodOrderer.Random.class)
 @ExtendWith(MockitoExtension.class)
 class CommandFactoryTest {
 
+    @InjectMocks
     private CommandFactory SUT;
 
+    @Mock
     private PostRepository postRepository;
+    @Mock
     private AuthorRepository authorRepository;
-
-    @BeforeEach
-    void setUp() {
-        postRepository = mock(PostRepository.class);
-        authorRepository = mock(AuthorRepository.class);
-        SUT = new CommandFactory(postRepository, authorRepository);
-    }
+    @Mock
+    private CategoryRepository categoryRepository;
 
     @Test
     void shouldReturnCorrectCommand() {
