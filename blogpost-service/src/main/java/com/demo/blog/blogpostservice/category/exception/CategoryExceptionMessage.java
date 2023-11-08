5cd76fa6bb063beeebbc1650f071c38455ececac
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-08 22:29:32 +0100

    Create enum class with static message fields for category exceptions

---
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/category/exception/CategoryExceptionMessage.java
@@ -0,0 +1,18 @@
+package com.demo.blog.blogpostservice.category.exception;
+
+public enum CategoryExceptionMessage {
+
+    NULL_NAME_MSG("Category name was null"),
+    NULL_ID_MSG("Category ID was null"),
+    NULL_REQUEST_MSG("Request was null");
+
+    final String message;
+
+    CategoryExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
