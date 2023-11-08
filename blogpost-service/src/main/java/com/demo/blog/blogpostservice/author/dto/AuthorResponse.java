76d891333770140aaca2e4ca01b64f17095df5a6
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-08 13:36:53 +0100

    Add a constructor

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/author/dto/AuthorResponse.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/author/dto/AuthorResponse.java
@@ -1,4 +1,10 @@
 package com.demo.blog.blogpostservice.author.dto;
 
+import com.demo.blog.blogpostservice.author.Author;
+
 public record AuthorResponse(String name) {
+
+    public AuthorResponse(Author author) {
+        this(author.getUsername());
+    }
 }
