21bda1e09508984d61cee174fca6ae833d257371
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-10 12:48:09 +0100

    Add test author reference

--- blogpost-service/src/test/java/com/demo/blog/blogpostservice/author/datasupply/AuthorDataSupply.java
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/author/datasupply/AuthorDataSupply.java
@@ -2,10 +2,10 @@ package com.demo.blog.blogpostservice.author.datasupply;
 
 import com.demo.blog.blogpostservice.author.Author;
 
-import static com.demo.blog.blogpostservice.datasupply.Constants.ANY_EMAIL;
-import static com.demo.blog.blogpostservice.datasupply.Constants.ANY_STRING;
+import static com.demo.blog.blogpostservice.datasupply.Constants.*;
 
 public class AuthorDataSupply {
 
-    public static final Author ANY_AUTHOR = new Author(ANY_STRING, ANY_EMAIL);
+    public static final Author ANY_AUTHOR = new Author(ANY_LONG, ANY_STRING, ANY_EMAIL, null);
+    public static final Author JOHN_SMITH = new Author(2L, "John Smith", "smith@mail.com", null);
 }
