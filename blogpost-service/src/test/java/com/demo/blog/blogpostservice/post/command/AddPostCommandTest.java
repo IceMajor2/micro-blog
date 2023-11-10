14d0da63d317e5be568da22fa9aa756252d31969
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-10 22:47:52 +0100

    Refactor

--- blogpost-service/src/test/java/com/demo/blog/blogpostservice/post/command/AddPostCommandTest.java
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/post/command/AddPostCommandTest.java
@@ -14,10 +14,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static com.demo.blog.blogpostservice.assertion.AllAssertions.assertThat;
 import static com.demo.blog.blogpostservice.author.datasupply.AuthorConstants.NULL_AUTHOR_MSG;
 import static com.demo.blog.blogpostservice.author.datasupply.AuthorDataSupply.ANY_AUTHOR;
-import static com.demo.blog.blogpostservice.datasupply.Constants.ANY_LONG;
 import static com.demo.blog.blogpostservice.post.datasupply.PostConstants.NULL_REQUEST_MSG;
 import static com.demo.blog.blogpostservice.post.datasupply.PostConstants.TITLE_EXISTS_MSG_T;
 import static com.demo.blog.blogpostservice.post.datasupply.PostDataSupply.SPRING_POST_REQUEST;
@@ -40,34 +38,19 @@ public class AddPostCommandTest {
     @MethodSource("com.demo.blog.blogpostservice.post.datasupply.PostDataSupply#validPostRequests")
     void shouldAddPost(PostRequest request) {
         // arrange
-        String expectedTitle = new String(request.title());
-        String expectedBody = new String(request.body());
-        long expectedAuthorId = ANY_AUTHOR.getId().longValue();
-
-        Post saveStub = new PostBuilder()
-                .withId(ANY_LONG)
-                .withTitle(request.title())
-                .withAuthor(ANY_AUTHOR.getId())
-                .withBody(request.body())
-                .publishedNow()
-                .build();
         Post postToSave = new PostBuilder()
                 .fromRequest(request)
                 .withAuthor(ANY_AUTHOR.getId())
                 .publishedNow()
                 .build();
-        when(postRepository.save(refEq(postToSave, "publishedOn"))).thenReturn(saveStub);
 
         SUT = new AddPostCommand(postRepository, request, ANY_AUTHOR);
 
         // act
-        Post actual = SUT.execute();
+        SUT.execute();
 
         // assert
-        assertThat(actual)
-                .isTitled(expectedTitle)
-                .writtenBy(expectedAuthorId)
-                .hasBody(expectedBody);
+        verify(postRepository, times(1)).save(refEq(postToSave, "publishedOn"));
     }
 
     @Test
