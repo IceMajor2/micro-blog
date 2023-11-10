fedf9309dbce8dc2bb4c70d472bc558c1c75efc5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-10 18:24:25 +0100

    Remove builder

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/category/dto/CategoryRequest.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/category/dto/CategoryRequest.java
@@ -1,10 +1,8 @@
 package com.demo.blog.blogpostservice.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
 
-@Builder
 public record CategoryRequest(
         @NotBlank(message = "Category name must be specified")
         @Length(max = 32, message = "Category name cannot exceed 32 characters")
