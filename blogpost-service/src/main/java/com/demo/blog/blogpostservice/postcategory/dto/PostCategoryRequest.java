a774f37c05ea0a1d724780c520ca8797f948d87d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-12 10:27:58 +0100

    Fix constraint types

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/postcategory/dto/PostCategoryRequest.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/postcategory/dto/PostCategoryRequest.java
@@ -1,12 +1,15 @@
 package com.demo.blog.blogpostservice.postcategory.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record PostCategoryRequest(
-        @NotBlank(message = "Post must be specified") Long postId,
-        @Min(value = 1, message = "At least one category must be specified") List<Long> categoryIds
+        @NotNull(message = "Post must be specified")
+        Long postId,
+        @NotNull(message = "At least one category must be specified")
+        @Size(min = 1, message = "At least one category must be specified")
+        List<Long> categoryIds
 ) {
 }
