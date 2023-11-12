140a887a1681cdfff37d5350412d40f61164972a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-12 10:15:36 +0100

    Change constraint

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/dto/PostBodyRequest.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/dto/PostBodyRequest.java
@@ -1,9 +1,10 @@
 package com.demo.blog.blogpostservice.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record PostBodyRequest(
-        @NotBlank(message = "Post must be specified")
+        @NotNull(message = "Post must be specified")
         Long postId,
         @NotBlank(message = "Body must be specified")
         String body
