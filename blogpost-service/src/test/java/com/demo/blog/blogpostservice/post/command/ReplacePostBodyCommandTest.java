baf95e74cb1a2688ce42c2dc912398671b1bae7a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-12 10:35:01 +0100

    Add test for NPE

--- blogpost-service/src/test/java/com/demo/blog/blogpostservice/post/command/ReplacePostBodyCommandTest.java
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/post/command/ReplacePostBodyCommandTest.java
@@ -3,6 +3,7 @@ package com.demo.blog.blogpostservice.post.command;
 import com.demo.blog.blogpostservice.post.Post;
 import com.demo.blog.blogpostservice.post.PostBuilder;
 import com.demo.blog.blogpostservice.post.PostRepository;
+import com.demo.blog.blogpostservice.post.datasupply.PostConstants;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -10,8 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static com.demo.blog.blogpostservice.datasupply.Constants.ANY_STRING;
 import static com.demo.blog.blogpostservice.post.datasupply.PostDataSupply.DOCKER_POST;
 import static com.demo.blog.blogpostservice.post.datasupply.PostDataSupply.NEW_DOCKER_BODY_REQUEST;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -35,5 +38,27 @@ class ReplacePostBodyCommandTest {
 
         // assert
         verify(postRepository, times(1)).save(expectedToSave);
+    }
+
+    @Test
+    void shouldThrowExceptionOnBodyNull() {
+        // arrange
+        SUT = new ReplacePostBodyCommand(postRepository, DOCKER_POST, null);
+
+        // act & assert
+        assertThatExceptionOfType(NullPointerException.class)
+                .isThrownBy(() -> SUT.execute())
+                .withMessage(PostConstants.BODY_BLANK_MSG);
+    }
+
+    @Test
+    void shouldThrowExceptionOnPostNull() {
+        // arrange
+        SUT = new ReplacePostBodyCommand(postRepository, null, ANY_STRING);
+
+        // act & assert
+        assertThatExceptionOfType(NullPointerException.class)
+                .isThrownBy(() -> SUT.execute())
+                .withMessage(PostConstants.NULL_POST_MSG);
     }
 }
