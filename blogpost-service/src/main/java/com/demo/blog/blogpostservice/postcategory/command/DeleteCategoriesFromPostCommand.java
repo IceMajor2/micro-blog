734636b2e5c08e723a4381ff946759bf97aa9018
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-15 14:14:29 +0100

    Require non-null components

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/postcategory/command/DeleteCategoriesFromPostCommand.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/postcategory/command/DeleteCategoriesFromPostCommand.java
@@ -1,12 +1,15 @@
 package com.demo.blog.blogpostservice.postcategory.command;
 
 import com.demo.blog.blogpostservice.category.Category;
+import com.demo.blog.blogpostservice.category.exception.CategoryExceptionMessage;
 import com.demo.blog.blogpostservice.command.Command;
 import com.demo.blog.blogpostservice.post.Post;
 import com.demo.blog.blogpostservice.post.PostRepository;
+import com.demo.blog.blogpostservice.post.exception.PostExceptionMessage;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class DeleteCategoriesFromPostCommand implements Command {
@@ -17,6 +20,8 @@ public class DeleteCategoriesFromPostCommand implements Command {
 
     @Override
     public Post execute() {
+        Objects.requireNonNull(post, PostExceptionMessage.NULL_POST_MSG.getMessage());
+        Objects.requireNonNull(categories, CategoryExceptionMessage.NULL_CATEGORIES_MSG.getMessage());
         categories.forEach(post::deleteCategory);
         return postRepository.save(post);
     }
