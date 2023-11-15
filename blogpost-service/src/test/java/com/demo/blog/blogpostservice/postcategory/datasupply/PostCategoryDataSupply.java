6e5b9506964e7a1601e667b010fc02a714944986
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-15 15:39:28 +0100

    Add test object

--- blogpost-service/src/test/java/com/demo/blog/blogpostservice/postcategory/datasupply/PostCategoryDataSupply.java
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/postcategory/datasupply/PostCategoryDataSupply.java
@@ -4,7 +4,7 @@ import com.demo.blog.blogpostservice.postcategory.dto.PostCategoryRequest;
 
 import java.util.List;
 
-import static com.demo.blog.blogpostservice.category.datasupply.CategoryDataSupply.CONTAINERS_CATEGORY;
+import static com.demo.blog.blogpostservice.category.datasupply.CategoryDataSupply.*;
 import static com.demo.blog.blogpostservice.post.datasupply.PostDataSupply.DOCKER_POST;
 import static com.demo.blog.blogpostservice.post.datasupply.PostDataSupply.SPRING_POST;
 
@@ -12,4 +12,7 @@ public class PostCategoryDataSupply {
 
     public static final PostCategoryRequest DOCKER_W_CONTAINERS_SPRING_REQ =
             new PostCategoryRequest(DOCKER_POST.getId(), List.of(CONTAINERS_CATEGORY.getId(), SPRING_POST.getId()));
+
+    public static final PostCategoryRequest SPRING_W_JAVA_SPRING_REQ =
+            new PostCategoryRequest(SPRING_POST.getId(), List.of(JAVA_CATEGORY.getId(), SPRING_CATEGORY.getId()));
 }
