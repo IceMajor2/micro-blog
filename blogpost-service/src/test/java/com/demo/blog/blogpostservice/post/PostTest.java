9fcd7868e34f321cdec195e1d86129e763e6e2f5
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-16 16:20:07 +0100

    Test 'deleteCategory' method

--- blogpost-service/src/test/java/com/demo/blog/blogpostservice/post/PostTest.java
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/post/PostTest.java
@@ -3,7 +3,9 @@ package com.demo.blog.blogpostservice.post;
 import org.junit.jupiter.api.Test;
 
 import static com.demo.blog.blogpostservice.assertion.AllAssertions.assertThat;
+import static com.demo.blog.blogpostservice.category.datasupply.CategoryDataSupply.CONTAINERS_CATEGORY;
 import static com.demo.blog.blogpostservice.category.datasupply.CategoryDataSupply.SPRING_CATEGORY;
+import static com.demo.blog.blogpostservice.post.datasupply.PostDataSupply.DOCKER_POST;
 import static com.demo.blog.blogpostservice.post.datasupply.PostDataSupply.SPRING_POST;
 
 class PostTest {
@@ -17,6 +19,9 @@ class PostTest {
 
         // act & assert
         assertThat(post.addCategory(SPRING_CATEGORY)).isFalse();
+        assertThat(post.getCategories())
+                .map(postCategory -> postCategory.getCategoryId().getId())
+                .contains(SPRING_CATEGORY.getId());
     }
 
     @Test
@@ -28,10 +33,36 @@ class PostTest {
 
         // act & assert
         assertThat(post.addCategory(SPRING_CATEGORY)).isTrue();
+        assertThat(post.getCategories())
+                .map(postCategory -> postCategory.getCategoryId().getId())
+                .contains(SPRING_CATEGORY.getId());
+    }
+
+    @Test
+    void shouldDoNothingOnDeletingNonAttachedCategory() {
+        // arrange
+        Post post = Post.PostBuilder.post(DOCKER_POST)
+                .clearCategories()
+                .build();
+
+        // act & assert
+        assertThat(post.deleteCategory(CONTAINERS_CATEGORY)).isFalse();
+        assertThat(post.getCategories())
+                .map(postCategory -> postCategory.getCategoryId().getId())
+                .doesNotContain(CONTAINERS_CATEGORY.getId());
     }
 
     @Test
     void shouldDeleteCategory() {
+        // arrange
+        Post post = Post.PostBuilder.post(SPRING_POST)
+                .setCategories(CONTAINERS_CATEGORY)
+                .build();
 
+        // act & assert
+        assertThat(post.deleteCategory(CONTAINERS_CATEGORY)).isTrue();
+        assertThat(post.getCategories())
+                .map(postCategory -> postCategory.getCategoryId().getId())
+                .doesNotContain(CONTAINERS_CATEGORY.getId());
     }
 }
