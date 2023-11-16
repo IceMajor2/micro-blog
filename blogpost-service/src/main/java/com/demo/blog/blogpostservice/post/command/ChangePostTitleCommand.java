599894e8f893caa39b206090bd0303ec2addde63
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-16 15:42:26 +0100

    Implement the least functionality to pass the test

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/ChangePostTitleCommand.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/ChangePostTitleCommand.java
@@ -14,6 +14,7 @@ public class ChangePostTitleCommand implements Command {
 
     @Override
     public Post execute() {
-        return null;
+        post.setTitle(newTitle);
+        return postRepository.save(post);
     }
 }
