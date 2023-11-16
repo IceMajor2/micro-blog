6b88ea0f2bc449f8a6601ef31554c5a30288b014
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-16 16:23:49 +0100

    Add constraints

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/dto/PostChangeTitleRequest.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/dto/PostChangeTitleRequest.java
@@ -1,8 +1,12 @@
 package com.demo.blog.blogpostservice.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 public record PostChangeTitleRequest(
-        @NotBlank(message = "Post title must be specified") String newTitle
+        @NotBlank(message = "Post title must be specified")
+        @Length(min = 5, message = "Post title must be at least 5 character long")
+        @Length(max = 255, message = "Post title cannot exceed 255 characters")
+        String newTitle
 ) {
 }
