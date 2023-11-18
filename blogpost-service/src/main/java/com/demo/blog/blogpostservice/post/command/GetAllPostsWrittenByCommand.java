fca5a15209e835614cd3240d045272c3c8bee7fb
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-18 14:25:16 +0100

    Return an ordered list of posts written by a given author

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/GetAllPostsWrittenByCommand.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/GetAllPostsWrittenByCommand.java
@@ -4,8 +4,10 @@ import com.demo.blog.blogpostservice.command.Command;
 import com.demo.blog.blogpostservice.post.Post;
 import com.demo.blog.blogpostservice.post.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Streamable;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class GetAllPostsWrittenByCommand implements Command<List<Post>> {
@@ -15,6 +17,8 @@ public class GetAllPostsWrittenByCommand implements Command<List<Post>> {
 
     @Override
     public List<Post> execute() {
-        return null;
+        return Streamable.of(postRepository.findByAuthorOrderByPublishedOnDesc(authorId))
+                .stream()
+                .collect(Collectors.toList());
     }
 }
