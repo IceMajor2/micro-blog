e32c523769c8d30b3f167a2d5a7a4e36580fcbc1
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-18 10:04:20 +0100

    Define command class

---
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/GetAllPostsWrittenByCommand.java
@@ -0,0 +1,20 @@
+package com.demo.blog.blogpostservice.post.command;
+
+import com.demo.blog.blogpostservice.command.Command;
+import com.demo.blog.blogpostservice.post.Post;
+import com.demo.blog.blogpostservice.post.PostRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class GetAllPostsWrittenByCommand implements Command {
+
+    private final PostRepository postRepository;
+    private final Long authorId;
+
+    @Override
+    public List<Post> execute() {
+        return null;
+    }
+}
