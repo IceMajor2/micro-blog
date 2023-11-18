7240adca72adf8e844cd70e676d212950af1df2a
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-18 14:32:09 +0100

    Throw exception on component null

--- blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/GetAllPostsWrittenByCommand.java
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/post/command/GetAllPostsWrittenByCommand.java
@@ -1,6 +1,7 @@
 package com.demo.blog.blogpostservice.post.command;
 
 import com.demo.blog.blogpostservice.author.Author;
+import com.demo.blog.blogpostservice.author.exception.AuthorExceptionMessage;
 import com.demo.blog.blogpostservice.command.Command;
 import com.demo.blog.blogpostservice.post.Post;
 import com.demo.blog.blogpostservice.post.PostRepository;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.util.Streamable;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class GetAllPostsWrittenByCommand implements Command<List<Post>> {
 
     @Override
     public List<Post> execute() {
+        Objects.requireNonNull(author, AuthorExceptionMessage.NULL_AUTHOR_MSG.getMessage());
         return Streamable.of(postRepository.findByAuthorOrderByPublishedOnDesc(author.getId()))
                 .stream()
                 .collect(Collectors.toList());
