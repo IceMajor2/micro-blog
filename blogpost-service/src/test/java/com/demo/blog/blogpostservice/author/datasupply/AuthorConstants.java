b154ec2b16b393594fbf550ab7ebc9587d388436
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-20 23:27:29 +0100

    Add test

--- blogpost-service/src/test/java/com/demo/blog/blogpostservice/author/datasupply/AuthorConstants.java
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/author/datasupply/AuthorConstants.java
@@ -2,5 +2,6 @@ package com.demo.blog.blogpostservice.author.datasupply;
 
 public class AuthorConstants {
 
+    public static final String USERNAME_BLANK_MSG = "Author username must be specified";
     public static final String NULL_AUTHOR_MSG = "Author was null";
 }
---
+++ blogpost-service/src/test/java/com/demo/blog/blogpostservice/author/dto/AuthorRequestTest.java
@@ -0,0 +1,30 @@
+package com.demo.blog.blogpostservice.author.dto;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+
+import static com.demo.blog.blogpostservice.assertion.AllAssertions.assertThat;
+import static com.demo.blog.blogpostservice.author.datasupply.AuthorConstants.USERNAME_BLANK_MSG;
+import static com.demo.blog.blogpostservice.datasupply.Constants.ANY_EMAIL;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+class AuthorRequestTest {
+
+    private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    @ParameterizedTest
+    @MethodSource("com.demo.blog.blogpostservice.datasupply.StringDataSupply#blankStrings")
+    @NullSource
+    void shouldThrowExceptionOnUsernameBlank(String blank) {
+        // act
+        AuthorRequest actual = new AuthorRequest(ANY_EMAIL, blank);
+
+        // assert
+        assertThat(validator.validate(actual)).containsExceptionMessages(USERNAME_BLANK_MSG);
+    }
+}
