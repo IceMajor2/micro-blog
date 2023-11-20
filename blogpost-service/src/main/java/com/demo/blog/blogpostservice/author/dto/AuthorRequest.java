96a81243c20c597e43626017d6f05769942f7e92
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-20 23:23:37 +0100

    Add request DTO

---
+++ blogpost-service/src/main/java/com/demo/blog/blogpostservice/author/dto/AuthorRequest.java
@@ -0,0 +1,15 @@
+package com.demo.blog.blogpostservice.author.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record AuthorRequest(
+        @Email
+        String email,
+        @NotBlank(message = "Author username must be specified")
+        @Length(min = 5, message = "Author username must be at least 5 characters long")
+        @Length(max = 32, message = "Author username cannot exceed 32 characters")
+        String username
+) {
+}
