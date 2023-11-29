44707f54df37de474b04334cc0cf2081c4abb223
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-29 20:04:49 +0100

    Initialize Aith client project

---
+++ blogpost-auth-client/src/main/java/com/demo/blog/blogpost/auth/client/BlogpostAuthClientApplication.java
@@ -0,0 +1,13 @@
+package com.demo.blog.blogpost.auth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostAuthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostAuthClientApplication.class, args);
+	}
+
+}
