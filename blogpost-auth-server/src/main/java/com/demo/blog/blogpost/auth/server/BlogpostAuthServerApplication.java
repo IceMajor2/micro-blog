ecf31ed4a4b78c625b10af29f29ac7fab4f48e6d
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-29 19:57:02 +0100

    Simple authorization server configuration to be extended

--- blogpost-auth-server/src/main/java/com/demo/blog/blogpost/auth/server/BlogpostAuthServerApplication.java
+++ blogpost-auth-server/src/main/java/com/demo/blog/blogpost/auth/server/BlogpostAuthServerApplication.java
@@ -2,12 +2,19 @@ package com.demo.blog.blogpost.auth.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BlogpostAuthServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogpostAuthServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogpostAuthServerApplication.class, args);
+    }
 
+    @Bean
+	PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
---
+++ blogpost-auth-server/src/main/java/com/demo/blog/blogpost/auth/server/config/UserConfig.java
@@ -0,0 +1,35 @@
+package com.demo.blog.blogpost.auth.server.config;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    ApplicationRunner userRunner(UserDetailsManager userDetailsManager, PasswordEncoder passwordEncoder) {
+        return args -> {
+            UserDetails admin = User.builder()
+                    .username("admin")
+                    .password(passwordEncoder.encode("admin"))
+                    .roles("admin")
+                    .build();
+            if (!userDetailsManager.userExists(admin.getUsername())) {
+                userDetailsManager.createUser(admin);
+            }
+        };
+    }
+}
