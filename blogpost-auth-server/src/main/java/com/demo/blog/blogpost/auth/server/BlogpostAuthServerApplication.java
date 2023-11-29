6861c2b20413a8882bae0f69efc2abf89275fde7
IceMajor2 <127668492+IceMajor2@users.noreply.github.com>
2023-11-29 19:48:16 +0100

    Add authorization server module

---
+++ blogpost-auth-server/src/main/java/com/demo/blog/blogpost/auth/server/BlogpostAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.blog.blogpost.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostAuthServerApplication.class, args);
+	}
+
+}
